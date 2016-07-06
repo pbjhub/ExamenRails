338c46a7b97ade3c6254c7214724b4b399a3e60b
pbjhub <af.munozes@gmail.com>
2016-07-05 19:59:38 -0500

    superman

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/batman'
-
-  root 'pages#batman'
+  get 'pages/superman'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
